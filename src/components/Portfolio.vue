917e7e4be6fb14746c94e24e7b7a26d449f9ffdb
Daniitoo <daniitoo.0102@gmail.com>
2021-03-16 23:45:43 -0500

    Add img index

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -150,47 +150,6 @@
           </div>
         </v-flex>
 
-        <v-flex class="cautro">
-          <v-dialog v-model="dialog" width="70%">
-            <template v-slot:activator="{ on }">
-              <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-            </template>
-            <v-card>
-              <v-img
-                position="top center"
-                border
-                class="imagen-muestra"
-                :src="workstate[id].imagen2"
-              ></v-img>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialog" width="70%">
-            <template v-slot:activator="{ on }">
-              <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-            </template>
-            <v-card>
-              <v-img
-                position="top center"
-                border
-                class="imagen-muestra"
-                :src="workstate[id].imagen1"
-              ></v-img>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialog" width="70%">
-            <template v-slot:activator="{ on }">
-              <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-            </template>
-            <v-card>
-              <v-img
-                position="top center"
-                border
-                class="imagen-muestra"
-                :src="img1"
-              ></v-img>
-            </v-card>
-          </v-dialog>
-        </v-flex>
       </v-layout>
     </div>
     <Theme />
